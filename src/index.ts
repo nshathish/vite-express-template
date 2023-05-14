a6dbf3cee086037f7d0b339e47f56eb55041e488
Shathish <470487+nshathish@users.noreply.github.com>
2023-05-14 19:35:21 +0100

    setup cors and json parser

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (_: Request, res: Response) => {
   res.json({ message: 'Hello World!' });
+});
+
+app.post('/', (req: Request, res: Response) => {
+  const { body } = req;
+  res.json({ message: `Hello ${body.name}!` });
 });
 
 if (import.meta.env.PROD) {
